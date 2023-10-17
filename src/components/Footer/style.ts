355edb6aa8c7d7e912714be83ae72b3a37dfadf8
GabrielBarrachino <gabrielbarrachino@outlook.com>
2023-10-17 16:25:00 -0300

    footer finalizado

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -2,11 +2,10 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
     width: 100%;
-    max-width: 164rem;
+    max-width: 122.2rem;
     margin: 0 auto;
     padding: 15rem 0 10rem 0;
     text-align: center;
-    display: none;
     h2{
         font-size: 5.8rem;
         font-weight: 400;
@@ -26,6 +25,34 @@ export const FooterContainer = styled.footer`
                     transform: scale(1.1);
                 }
             }
+        }
+    }
+
+    @media (max-width: 730px){
+        padding: 8rem 1.5rem;
+        h2{
+            font-size: 4.5rem;
+        }
+        nav{
+            ul{
+                gap: 1rem;
+            }
+        }
+    }
+    @media (max-width: 540px){
+        padding: 5rem 1.5rem;
+        h2{
+            font-size: 3rem;
+        }
+        nav{
+            ul{
+                flex-wrap: wrap;
+            }
+        }
+    }
+    @media (max-width: 400px){
+        h2{
+            font-size: 2.5rem;
         }
     }
 `
